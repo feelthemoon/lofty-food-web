dd94acf3ded5a60c6d4813ea9b659e63e5392bb2
Vladislav <programmer28.vdovin@yandex.ru>
2021-10-22 23:55:25 +0300

    feat: added ui homepage

---
+++ client/src/components/Header.vue
@@ -0,0 +1,13 @@
+<template>
+  <header class="header">
+    <img src="@/assets/static/img/logo.svg" alt="Lofty Food" />
+  </header>
+</template>
+
+<script>
+export default {
+  name: 'Header',
+};
+</script>
+
+<style scoped></style>
--- client/src/router/index.js
+++ client/src/router/index.js
@@ -1,25 +1,19 @@
-import { createRouter, createWebHistory } from 'vue-router'
-import Home from '../views/Home.vue'
+import { createRouter, createWebHistory } from 'vue-router';
 
 const routes = [
   {
     path: '/',
-    name: 'Home',
-    component: Home
+    component: () => import(/* webpackChunkName: "main" */ '@/views/Main.vue'),
   },
   {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-  }
-]
+    path: '/signin',
+    component: () => import(/* webpackChunkName: "auth" */ '@/views/Auth.vue'),
+  },
+];
 
 const router = createRouter({
   history: createWebHistory(process.env.BASE_URL),
-  routes
-})
+  routes,
+});
 
-export default router
+export default router;
