76705b3e628066a1cb0746f8e061d6a97df41e30
Vladislav <programmer28.vdovin@yandex.ru>
2021-10-23 22:10:46 +0300

    fix: now table returns only by day food

--- server/src/food/food.controller.ts
+++ server/src/food/food.controller.ts
@@ -10,17 +10,14 @@ export class FoodController {
     private readonly foodService: FoodService,
     private tableParser: TableParser,
   ) {}
-  @Get('/table/:day/:page')
+  @Get('/table/:day')
   async getTableForClient(@Req() req: Request, @Res() res: Response) {
     try {
       await this.tableParser.downloadFile(
         process.env.APP_REMOTE_URL,
         process.env.OLD_TABLE,
       );
-      const data = this.tableParser.readTableByDayAndPage(
-        +req.params.day,
-        +req.params.page,
-      );
+      const data = this.tableParser.readTableByDay(+req.params.day);
       res.status(200).json(data);
     } catch (e) {
       res.status(500).json({ message: 'Wrong data' });
