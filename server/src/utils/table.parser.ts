3313ac334ffe13a2b3c2ff8aca2d2f800a91c5fc
Vladislav <programmer28.vdovin@yandex.ru>
2021-10-23 22:10:46 +0300

    fix: now table returns only by day food

--- server/src/utils/table.parser.ts
+++ server/src/utils/table.parser.ts
@@ -11,7 +11,6 @@ const writeFilePromise = promisify(writeFile);
 @Injectable()
 export class TableParser {
   private table: any;
-  private totalPerPage = 25;
   constructor(private readonly httpService: HttpService) {}
 
   async downloadFile(url, outputPath) {
@@ -27,22 +26,46 @@ export class TableParser {
     }
   }
 
-  readTableByDayAndPage(day: number, page: number) {
-    let currentDayFood = this.table[day - 1].data.map((row) => {
-      if (typeof row[1] === 'string' && typeof row[3] === 'number') {
-        row = { id: row[0], title: row[1], price: row[3] };
+  readTableByDay(day: number) {
+    // parse from table all data what we need by current day
+    let currentDayFood = this.table[day - 1].data
+      .map((row) => {
+        if (typeof row[1] === 'string' && typeof row[3] === 'number') {
+          row = {
+            id: row[0],
+            title: row[1],
+            price: row[3],
+          };
+        }
+        return row;
+      })
+      .slice(2, -5);
+
+    // define food types like in table
+    const foodTypes = currentDayFood
+      .map((item) => {
+        if (Array.isArray(item)) {
+          return item[0];
+        }
+        return null;
+      })
+      .filter((item) => item);
+
+    //delete first item for correct work next cycle
+    currentDayFood.shift();
+
+    const food = {};
+    let index = 0;
+
+    //generate new array of objects where keys is food types and values is food objects by current day
+    currentDayFood.forEach((item) => {
+      if (!Array.isArray(item)) {
+        return food[foodTypes[index]]?.length
+          ? food[foodTypes[index]].push(item)
+          : (food[foodTypes[index]] = [item]);
       }
-      return row;
+      index++;
     });
-    currentDayFood = currentDayFood.filter((item) => !Array.isArray(item));
-    return {
-      data: [
-        ...currentDayFood.slice(
-          (page - 1) * this.totalPerPage,
-          page * this.totalPerPage,
-        ),
-      ],
-      total: currentDayFood.length,
-    };
+    return food;
   }
 }
