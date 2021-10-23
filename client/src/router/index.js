0789bc0ab50bdceff987e0a1cc7732051b78c59d
Vladislav <programmer28.vdovin@yandex.ru>
2021-10-23 17:30:11 +0300

    feat: migrate from vue3 to vue2

--- client/src/router/index.js
+++ client/src/router/index.js
@@ -1,19 +1,29 @@
-import { createRouter, createWebHistory } from 'vue-router';
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+import Home from '../views/Home.vue'
+
+Vue.use(VueRouter)
 
 const routes = [
   {
     path: '/',
-    component: () => import(/* webpackChunkName: "main" */ '@/views/Main.vue'),
+    name: 'Home',
+    component: Home
   },
   {
-    path: '/signin',
-    component: () => import(/* webpackChunkName: "auth" */ '@/views/Auth.vue'),
-  },
-];
+    path: '/about',
+    name: 'About',
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+  }
+]
 
-const router = createRouter({
-  history: createWebHistory(process.env.BASE_URL),
-  routes,
-});
+const router = new VueRouter({
+  mode: 'history',
+  base: process.env.BASE_URL,
+  routes
+})
 
-export default router;
+export default router
