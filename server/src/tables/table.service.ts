a9f28c3d39395b9f83d012a6745873ca1b7c1862
Vladislav Vdovin <programmer28.vdovin@yandex.ru>
2021-11-20 02:00:41 +0300

    added docker file refactor crons

--- server/src/tables/table.service.ts
+++ server/src/tables/table.service.ts
@@ -14,14 +14,14 @@ export class TableService {
 
   constructor(private readonly httpService: HttpService) {}
 
-  @Cron('0 02 17 * * */5')
+  @Cron('0 00 17 * * */4')
   private async downloadCron() {
     if (existsSync(process.env.OLD_TABLE)) {
       unlinkSync(process.env.OLD_TABLE);
     }
     await this.downloadFile(process.env.APP_REMOTE_URL, process.env.OLD_TABLE);
   }
-  @Cron('0 03 17 * * */5')
+  @Cron('0 05 15 * * */4')
   private async createTableCron() {
     if (existsSync(process.env.NEW_TABLE)) {
       unlinkSync(process.env.NEW_TABLE);
