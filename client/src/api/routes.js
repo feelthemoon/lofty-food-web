3a4891afd9fc61a78dea6715b1a8df5ff4fb3449
Vladislav <33966273+feelthemoon@users.noreply.github.com>
2021-12-21 13:46:39 +0300

    fixed: endpoints && redirect slack uri (#16)

--- client/src/api/routes.js
+++ client/src/api/routes.js
@@ -1,10 +1,10 @@
 export default {
   table(day) {
-    return `table/${day}/`;
+    return `api/table/${day}/`;
   },
-  sendTable: 'postdata',
+  sendTable: 'api/postdata',
   auth: code =>
     `https://slack.com/api/openid.connect.token?client_id=${process.env.VUE_APP_CLIENT_ID}&client_secret=${process.env.VUE_APP_CLIENT_SECRET}&code=${code}`,
-  userInfo: token => `user`,
-  users: 'all',
+  userInfo: token => `api/user`,
+  users: 'api/all',
 };
