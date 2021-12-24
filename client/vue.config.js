cb7c5f96065d74c6562344493f38e093ca5c2d1b
Vladislav Vdovin <programmer28.vdovin@yandex.ru>
2021-12-24 03:59:23 +0300

    feat: added cart component && fix: fixed styles && refactor vue.config.js

--- client/vue.config.js
+++ client/vue.config.js
@@ -1,13 +1,59 @@
+const path = require('path');
 module.exports = {
   transpileDependencies: [
     'vuetify'
   ],
   runtimeCompiler: true,
+  filenameHashing: true,
+  productionSourceMap: false,
   css: {
+    extract: true,
     loaderOptions: {
       sass: {
         implementation: require('sass'),
       },
     },
-  }
+  },
+  pages: {
+    app: {
+      entry: 'src/main.js',
+      template: 'public/index.html',
+      filename: './public/index.html',
+      chunks: ["chunk-vender", "chunk-common", "chunk-app-vendors", "app"]
+    },
+  },
+    configureWebpack(config) {
+    const IS_VENDOR = /[\\/]node_modules[\\/]/;
+    config.optimization.splitChunks = {
+      cacheGroups: {
+        app: {
+          name: `chunk-app-vendors`,
+          priority: -11,
+          chunks: (chunk) => chunk.name === 'app',
+          test: IS_VENDOR,
+          enforce: true,
+        },
+        common: {
+          name: 'chunk-common',
+          priority: -20,
+          chunks: 'initial',
+          minChunks: 2,
+          reuseExistingChunk: true,
+          enforce: true,
+        },
+      },
+    };
+    return {
+      output: {
+        hotUpdateChunkFilename: 'hot/hot-update.js',
+        hotUpdateMainFilename: 'hot/hot-update.json',
+      },
+      resolve: {
+        alias: {
+          '@': path.resolve(__dirname, './src/'),
+        },
+        extensions: ['.js', '.vue'],
+      },
+    };
+  },
 }
