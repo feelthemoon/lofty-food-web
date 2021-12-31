b6438c6fede2f33cd7af2395ee786a6964b26e6b
Vladislav Vdovin <programmer28.vdovin@yandex.ru>
2021-12-31 19:36:26 +0300

    fix: fixed duplicating orders

--- client/src/views/Auth.vue
+++ client/src/views/Auth.vue
@@ -2,7 +2,7 @@
   <div class="auth">
     <h3>Заходи и забирай свое хрючево!😃</h3>
     <a
-      href="https://slack.com/openid/connect/authorize?scope=openid%20email%20profile&amp;response_type=code&amp;redirect_uri=https%3A%2F%2Fac47-77-51-68-107.ngrok.io&amp;client_id=14172408775.2637076670294"
+      href="https://slack.com/openid/connect/authorize?scope=openid%20profile%20email&amp;response_type=code&amp;redirect_uri=https%3A%2F%2Floftyfood.ru%2F&amp;client_id=14172408775.2637076670294"
       style="
         align-items: center;
         color: #fff !important;
